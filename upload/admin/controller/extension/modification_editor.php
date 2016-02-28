a079f2854d6f6961624051097f08ece3aa16a5b0
Manoel Vidal <manoel.vidal@opencartbrasil.com.br>
2016-02-28 15:04:03 -0300

    Versão 1.3

--- upload/admin/controller/extension/modification_editor.php
+++ upload/admin/controller/extension/modification_editor.php
@@ -5,68 +5,75 @@ class ControllerExtensionModificationEditor extends Controller {
 			$this->response->redirect($this->url->link('extension/modification', 'token=' . $this->session->data['token'], 'SSL'));
 		}
 
+		$this->load->language('extension/modification_editor');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$this->document->addScript('view/javascript/ace/ace.js');
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], 'SSL')
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_modifications'),
+			'href' => $this->url->link('extension/modification', 'token=' . $this->session->data['token'], 'SSL')
+		);
+
 		if (isset($this->request->get['modification_id'])) {
 			$modification_id = $this->request->get['modification_id'];
+
+			$this->load->model('extension/modification_editor');
+			$modification_info = $this->model_extension_modification_editor->getModification($modification_id);
+
+			if ($modification_info) {
+				$data['modification_id'] = $modification_id;
+				$data['name'] = $modification_info['name'];
+				$data['xml'] = $modification_info['xml'];
+
+				$data['breadcrumbs'][] = array(
+					'text' => $this->language->get('heading_title'),
+					'href' => $this->url->link('extension/modification_editor', 'token=' . $this->session->data['token'] . '&modification_id=' . $modification_id, 'SSL')
+				);
+			} else {
+				$this->response->redirect($this->url->link('extension/modification', 'token=' . $this->session->data['token'], 'SSL'));
+			}
 		} else {
-			$modification_id = 0;
-		}
-
-		$this->load->model('extension/modification_editor');
-		$modification_info = $this->model_extension_modification_editor->getModification($modification_id);
-
-		if ($modification_info) {
-			$this->load->language('extension/modification_editor');
-
-			$this->document->setTitle($this->language->get('heading_title'));
-
-			$this->document->addScript('view/javascript/ace/ace.js');
-
-			$data['modification_id'] = $modification_id;
-			$data['name'] = $modification_info['name'];
-			$data['xml'] = $modification_info['xml'];
-
-			$data['breadcrumbs'] = array();
-
-			$data['breadcrumbs'][] = array(
-				'text' => $this->language->get('text_home'),
-				'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], 'SSL')
-			);
-
-			$data['breadcrumbs'][] = array(
-				'text' => $this->language->get('text_modifications'),
-				'href' => $this->url->link('extension/modification', 'token=' . $this->session->data['token'], 'SSL')
-			);
+			$data['modification_id'] = 0;
+			$data['name'] = $this->language->get('text_new');
+			$data['xml'] = '';
 
 			$data['breadcrumbs'][] = array(
 				'text' => $this->language->get('heading_title'),
-				'href' => $this->url->link('extension/modification_editor', 'token=' . $this->session->data['token'] . '&modification_id=' . $modification_id, 'SSL')
+				'href' => $this->url->link('extension/modification_editor', 'token=' . $this->session->data['token'], 'SSL')
 			);
-
-			$data['heading_title'] = $this->language->get('heading_title');
-
-			$data['entry_xml_code'] = $this->language->get('entry_xml_code');
-
-			$data['text_loading'] = $this->language->get('text_loading');
-			$data['text_erasing'] = $this->language->get('text_erasing');
-
-			$data['button_clear_data']  = $this->language->get('button_clear_data');
-			$data['button_clear_image'] = $this->language->get('button_clear_image');
-
-			$data['button_save'] = $this->language->get('button_save');
-			$data['button_return'] = $this->language->get('button_return');
-
-			$data['return'] = $this->url->link('extension/modification', 'token=' . $this->session->data['token'], 'SSL');
-			
-			$data['token'] = $this->session->data['token'];
-
-			$data['header'] = $this->load->controller('common/header');
-			$data['column_left'] = $this->load->controller('common/column_left');
-			$data['footer'] = $this->load->controller('common/footer');
-
-			$this->response->setOutput($this->load->view('extension/modification_editor.tpl', $data));
-		} else {
-			$this->response->redirect($this->url->link('extension/modification', 'token=' . $this->session->data['token'], 'SSL'));
 		}
+
+		$data['heading_title'] = $this->language->get('heading_title');
+
+		$data['entry_xml_code'] = $this->language->get('entry_xml_code');
+
+		$data['text_loading'] = $this->language->get('text_loading');
+		$data['text_erasing'] = $this->language->get('text_erasing');
+
+		$data['button_clear_data']  = $this->language->get('button_clear_data');
+		$data['button_clear_image'] = $this->language->get('button_clear_image');
+
+		$data['button_save'] = $this->language->get('button_save');
+		$data['button_return'] = $this->language->get('button_return');
+
+		$data['return'] = $this->url->link('extension/modification', 'token=' . $this->session->data['token'], 'SSL');
+		
+		$data['token'] = $this->session->data['token'];
+
+		$data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['footer'] = $this->load->controller('common/footer');
+
+		$this->response->setOutput($this->load->view('extension/modification_editor.tpl', $data));
 	}
 
 	public function save() {
@@ -76,73 +83,79 @@ class ControllerExtensionModificationEditor extends Controller {
 
 		if (!$this->user->hasPermission('modify', 'extension/modification_editor')) {
 			$json['error'] = $this->language->get('error_permission');
-		} else {	
-			if (isset($this->request->post['modification_id'])) {
-				$modification_id = $this->request->post['modification_id'];
-			} else {
-				$modification_id = 0;
-			}
+		} else {
+			$xml = html_entity_decode($this->request->post['xml']);
 
-			$this->load->model('extension/modification_editor');
-			$modification_info = $this->model_extension_modification_editor->getModification($modification_id);
+			if ($xml) {
+				try {
+					$dom = new DOMDocument('1.0', 'UTF-8');
+					$dom->loadXml($xml);
+					
+					$name = $dom->getElementsByTagName('name')->item(0);
 
-			if ($modification_info) {
-				$xml = html_entity_decode($this->request->post['xml']);
+					if ($name) {
+						$name = $name->nodeValue;
+					} else {
+						$name = '';
+					}
+					
+					$author = $dom->getElementsByTagName('author')->item(0);
 
-				if ($xml) {
-					try {
-						$dom = new DOMDocument('1.0', 'UTF-8');
-						$dom->loadXml($xml);
-						
-						$name = $dom->getElementsByTagName('name')->item(0);
+					if ($author) {
+						$author = $author->nodeValue;
+					} else {
+						$author = '';
+					}
 
-						if ($name) {
-							$name = $name->nodeValue;
+					$version = $dom->getElementsByTagName('version')->item(0);
+
+					if ($version) {
+						$version = $version->nodeValue;
+					} else {
+						$version = '';
+					}
+
+					$link = $dom->getElementsByTagName('link')->item(0);
+
+					if ($link) {
+						$link = $link->nodeValue;
+					} else {
+						$link = '';
+					}
+
+					$modification_data = array(
+						'name'            => $name,
+						'author'          => $author,
+						'version'         => $version,
+						'link'            => $link,
+						'xml'             => $xml
+					);
+
+					if (isset($this->request->post['modification_id'])) {
+						$modification_id = $this->request->post['modification_id'];
+
+						if ($modification_id > 0) {
+							$this->load->model('extension/modification_editor');
+							$modification_info = $this->model_extension_modification_editor->getModification($modification_id);
+
+							if ($modification_info) {
+								$this->model_extension_modification_editor->editModification($modification_id, $modification_data);
+							} else {
+								$json['error'] = $this->language->get('error_modification');
+							}
 						} else {
-							$name = '';
+							$this->model_extension_modification_editor->addModification($modification_data);
 						}
-						
-						$author = $dom->getElementsByTagName('author')->item(0);
-
-						if ($author) {
-							$author = $author->nodeValue;
-						} else {
-							$author = '';
-						}
-
-						$version = $dom->getElementsByTagName('version')->item(0);
-
-						if ($version) {
-							$version = $version->nodeValue;
-						} else {
-							$version = '';
-						}
-
-						$link = $dom->getElementsByTagName('link')->item(0);
-
-						if ($link) {
-							$link = $link->nodeValue;
-						} else {
-							$link = '';
-						}
-
-						$modification_data = array(
-							'modification_id' => $modification_id,
-							'name'            => $name,
-							'author'          => $author,
-							'version'         => $version,
-							'link'            => $link,
-							'xml'             => $xml
-						);
 
 						if (!$json) {
-							$this->model_extension_modification_editor->editModification($modification_data);
 							$json['success'] = $this->language->get('text_success');
 							$this->refresh();
 						}
-					} catch(Exception $exception) {
-						$json['error'] = sprintf($this->language->get('error_exception'), $exception->getCode(), $exception->getMessage(), $exception->getFile(), $exception->getLine());
+					} else {
+						$json['error'] = $this->language->get('error_modification');
 					}
+				} catch(Exception $exception) {
+					$json['error'] = sprintf($this->language->get('error_exception'), $exception->getCode(), $exception->getMessage(), $exception->getFile(), $exception->getLine());
 				}
 			} else {
 				$json['error'] = $this->language->get('error_file');
@@ -169,19 +182,29 @@ class ControllerExtensionModificationEditor extends Controller {
 
 		if ($modification) {
 			$filename = $modification['code'] . '.ocmod.xml';
+			$file = $modification['xml'];
+
+			ob_start();
+			echo $file;
+			$download = ob_get_contents();
+			$size = ob_get_length();
+			ob_end_clean();
 			
 			if (!headers_sent()) {
 				if (!empty($modification['xml'])) {
 					header('Content-Type: application/octet-stream');
-					header('Content-Description: File Transfer');
 					header('Content-Disposition: attachment; filename="' . $filename . '"');
 					header('Content-Transfer-Encoding: binary');
 					header('Expires: 0');
 					header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 					header('Pragma: public');
-					header('Content-Length: ' . utf8_strlen($filename));
+					header('Content-Length: ' . $size);
 
-					echo $modification['xml'];
+					if (ob_get_level()) {
+						ob_end_clean();
+					}
+
+					echo $download;
 
 					exit();
 				} else {
