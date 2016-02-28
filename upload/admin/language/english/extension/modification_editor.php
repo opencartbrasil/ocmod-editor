6c35282811cc2d345dac8092e2a4cc266bff1734
Manoel Vidal <manoel.vidal@opencartbrasil.com.br>
2016-02-28 15:04:43 -0300

    Versão 1.3

--- upload/admin/language/english/extension/modification_editor.php
+++ upload/admin/language/english/extension/modification_editor.php
@@ -9,11 +9,13 @@ $_['text_clear_image']   = 'Success: Erased images cache!';
 $_['text_loading']       = 'Saving...';
 $_['text_erasing']       = 'Erasing...';
 $_['text_modifications'] = 'Modifications';
+$_['text_new']           = 'New modification';
 
 // Entry
 $_['entry_xml_code']     = 'XML Code';
 
 // Button
+$_['button_new']         = 'New modification';
 $_['button_edit']        = 'Edit modification';
 $_['button_download']    = 'Download xml';
 $_['button_clear_data']  = 'Clear cache database file';
@@ -23,5 +25,6 @@ $_['button_return']      = 'Return modifications';
 
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify Modifications!';
+$_['error_modification'] = 'Modification Id is not valid!';
 $_['error_file']         = 'XML File could not be found!';
 $_['error_headers']      = 'Error: Headers already sent out!';
