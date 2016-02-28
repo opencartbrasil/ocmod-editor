bdfbff8ea77e0595636b10f0565ded775cf5c8cc
Manoel Vidal <manoel.vidal@opencartbrasil.com.br>
2016-02-28 15:05:16 -0300

    Versão 1.3

--- upload/admin/language/portuguese-br/extension/modification_editor.php
+++ upload/admin/language/portuguese-br/extension/modification_editor.php
@@ -9,11 +9,13 @@ $_['text_clear_image']   = 'O cache de imagens foi apagado!';
 $_['text_loading']       = 'Salvando...';
 $_['text_erasing']       = 'Apagando...';
 $_['text_modifications'] = 'Modificações';
+$_['text_new']           = 'Nova modificação';
 
 // Entry
 $_['entry_xml_code']     = 'Código XML';
 
 // Button
+$_['button_new']         = 'Nova modificação';
 $_['button_edit']        = 'Editar modificação';
 $_['button_download']    = 'Download do xml';
 $_['button_clear_data']  = 'Apagar cache de dados';
@@ -23,5 +25,6 @@ $_['button_return']      = 'Voltar para modificações';
 
 // Error
 $_['error_permission']   = 'Atenção: Você não possui permissão para editar as Modificações!';
+$_['error_modification'] = 'O Id da modificação não é válido!';
 $_['error_file']         = 'O arquivo xml não pode ser encontrado!';
 $_['error_headers']      = 'Erro: Os cabeçalhos já foram enviados!';
