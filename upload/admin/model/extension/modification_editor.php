f27b3f766f4033678b17b37593c2f482ed90f721
Manoel Vidal <manoel.vidal@opencartbrasil.com.br>
2016-02-28 15:05:50 -0300

    Versão 1.3

--- upload/admin/model/extension/modification_editor.php
+++ upload/admin/model/extension/modification_editor.php
@@ -5,6 +5,10 @@ class ModelExtensionModificationEditor extends Model {
 
 		return $query->row;
 	}
+	
+	public function addModification($data) {
+		$this->db->query("INSERT INTO " . DB_PREFIX . "modification SET name = '" . $this->db->escape($data['name']) . "', author = '" . $this->db->escape($data['author']) . "', version = '" . $this->db->escape($data['version']) . "', link = '" . $this->db->escape($data['link']) . "', xml = '" . $this->db->escape($data['xml']) . "'");
+	}
 
 	public function editModification($data) {
 		$this->db->query("UPDATE " . DB_PREFIX . "modification SET name = '" . $this->db->escape($data['name']) . "', author = '" . $this->db->escape($data['author']) . "', version = '" . $this->db->escape($data['version']) . "', link = '" . $this->db->escape($data['link']) . "', xml = '" . $this->db->escape($data['xml']) . "' WHERE modification_id = '" . (int)$data['modification_id'] . "'");
