85e74c53a6e7bdc770943b636e4cceb63c79d266
Manoel Vidal <manoel.vidal@opencartbrasil.com.br>
2016-04-06 13:20:15 -0300

    Versão 1.4

---
+++ upload/admin/language/en-gb/extension/modification_editor.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']      = 'Modification Editor';
+
+// Text
+$_['text_success_edit']  = 'Success: You have modified modification!';
+$_['text_success_add']   = 'Success: You have add modification!';
+$_['text_clear_data']    = 'Success: Erased database file cache!';
+$_['text_clear_image']   = 'Success: Erased images cache!';
+$_['text_loading']       = 'Saving...';
+$_['text_erasing']       = 'Erasing...';
+$_['text_modifications'] = 'Modifications';
+$_['text_new']           = 'New modification';
+
+// Entry
+$_['entry_xml_code']     = 'XML Code';
+
+// Button
+$_['button_new']         = 'New modification';
+$_['button_edit']        = 'Edit modification';
+$_['button_download']    = 'Download xml';
+$_['button_clear_data']  = 'Clear cache database file';
+$_['button_clear_image'] = 'Clear cache images';
+$_['button_save']        = 'Save modification';
+$_['button_return']      = 'Return modifications';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify Modifications!';
+$_['error_code']         = 'Modification requires a unique ID code!';
+$_['error_exists']       = 'Modification %s is using the same ID code!';
+$_['error_modification'] = 'Modification Id is not valid!';
+$_['error_file']         = 'XML File could not be found!';
+$_['error_headers']      = 'Error: Headers already sent out!';
---
+++ upload/admin/language/pt-br/extension/modification_editor.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']      = 'Editor de modificações';
+
+// Text
+$_['text_success_edit']  = 'Modificação alterada com sucesso!';
+$_['text_success_add']   = 'Modificação incluída com sucesso!';
+$_['text_clear_data']    = 'O cache de arquivos do banco de dados foi apagado!';
+$_['text_clear_image']   = 'O cache de imagens foi apagado!';
+$_['text_loading']       = 'Salvando...';
+$_['text_erasing']       = 'Apagando...';
+$_['text_modifications'] = 'Modificações';
+$_['text_new']           = 'Nova modificação';
+
+// Entry
+$_['entry_xml_code']     = 'Código XML';
+
+// Button
+$_['button_new']         = 'Nova modificação';
+$_['button_edit']        = 'Editar modificação';
+$_['button_download']    = 'Download do xml';
+$_['button_clear_data']  = 'Apagar cache de dados';
+$_['button_clear_image'] = 'Apagar cache de imagens';
+$_['button_save']        = 'Salvar modificação';
+$_['button_return']      = 'Voltar para modificações';
+
+// Error
+$_['error_permission']   = 'Atenção: Você não possui permissão para inserir ou editar as Modificações!';
+$_['error_code']         = 'Você não definiu um CODE no XML!';
+$_['error_exists']       = 'O CODE utilizado já está em uso pela extensão %s!';
+$_['error_modification'] = 'O ID da modificação não é válido!';
+$_['error_file']         = 'O arquivo xml não pode ser encontrado!';
+$_['error_headers']      = 'Erro: Os cabeçalhos já foram enviados!';
