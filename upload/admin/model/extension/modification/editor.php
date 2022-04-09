5587779424e53fe43c670bc5ca715db177467e61
Manoel Vidal <manoel@v5market.com>
2022-04-09 12:36:14 -0300

    fix: correções no model

--- upload/admin/model/extension/modification/editor.php
+++ upload/admin/model/extension/modification/editor.php
@@ -1,7 +1,7 @@
 <?php
 class ModelExtensionModificationEditor extends Model {
-    public function getModifications($data = array()) {
-        $sql = "SELECT * FROM " . DB_PREFIX . "modification ORDER BY name";
+    public function getModifications() {
+        $sql = "SELECT * FROM `" . DB_PREFIX . "modification` ORDER BY name";
 
         $query = $this->db->query($sql);
 
@@ -9,35 +9,36 @@ class ModelExtensionModificationEditor extends Model {
     }
 
     public function getModification($modification_id) {
-        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "modification WHERE modification_id = '" . (int)$modification_id . "'");
+        $query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "modification` WHERE modification_id = '" . (int) $modification_id . "'");
 
         return $query->row;
     }
 
     public function getModificationByCode($code) {
-        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "modification WHERE code = '" . $this->db->escape($code) . "'");
+        $query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "modification` WHERE code = '" . $this->db->escape($code) . "'");
 
         return $query->row;
     }
 
     public function addModification($data) {
-        $this->db->query("INSERT INTO " . DB_PREFIX . "modification SET code = '" . $this->db->escape($data['code']) . "', name = '" . $this->db->escape($data['name']) . "', author = '" . $this->db->escape($data['author']) . "', version = '" . $this->db->escape($data['version']) . "', link = '" . $this->db->escape($data['link']) . "', xml = '" . $this->db->escape($data['xml']) . "', status = '" . (int)$data['status'] . "', date_added = NOW()");
+        $this->db->query("INSERT INTO `" . DB_PREFIX . "modification` SET code = '" . $this->db->escape($data['code']) . "', name = '" . $this->db->escape($data['name']) . "', author = '" . $this->db->escape($data['author']) . "', version = '" . $this->db->escape($data['version']) . "', link = '" . $this->db->escape($data['link']) . "', xml = '" . $this->db->escape($data['xml']) . "', status = '" . (int) $data['status'] . "', date_added = NOW()");
     }
 
     public function editModification($modification_id, $data) {
-        $this->db->query("UPDATE " . DB_PREFIX . "modification SET code = '" . $this->db->escape($data['code']) . "', name = '" . $this->db->escape($data['name']) . "', author = '" . $this->db->escape($data['author']) . "', version = '" . $this->db->escape($data['version']) . "', link = '" . $this->db->escape($data['link']) . "', xml = '" . $this->db->escape($data['xml']) . "' WHERE modification_id = '" . (int)$modification_id . "'");
+        $this->db->query("UPDATE `" . DB_PREFIX . "modification` SET code = '" . $this->db->escape($data['code']) . "', name = '" . $this->db->escape($data['name']) . "', author = '" . $this->db->escape($data['author']) . "', version = '" . $this->db->escape($data['version']) . "', link = '" . $this->db->escape($data['link']) . "', xml = '" . $this->db->escape($data['xml']) . "' WHERE modification_id = '" . (int) $modification_id . "'");
     }
 
     public function getTotalSearchModificationElement($data = array()) {
-        $sql = "SELECT COUNT(*) AS total FROM " . DB_PREFIX . "modification";
+        $sql = "SELECT COUNT(*) AS total FROM `" . DB_PREFIX . "modification`";
 
         $implode = array();
 
         if (!empty($data['search_query'])) {
             $query_search = htmlspecialchars_decode($data['search_query']);
             $search_exploded = explode ( " ", $query_search );
+
             foreach( $search_exploded as $search_each ) {
-                $implode[] = "xml LIKE '%" . $this->db->escape($search_each) . "%'";
+                $implode[] = " xml LIKE '%" . $this->db->escape($search_each) . "%'";
             }
         }
 
@@ -51,15 +52,16 @@ class ModelExtensionModificationEditor extends Model {
     }
 
     public function searchModificationElement($data = array()) {
-        $sql = "SELECT * FROM " . DB_PREFIX . "modification";
+        $sql = "SELECT * FROM `" . DB_PREFIX . "modification`";
 
         $implode = array();
 
         if (!empty($data['search_query'])) {
             $query_search = htmlspecialchars_decode($data['search_query']);
-            $search_exploded = explode( " ", $query_search );
+            $search_exploded = explode(" ", $query_search);
+
             foreach( $search_exploded as $search_each ) {
-                $implode[] = "xml LIKE '%" . $this->db->escape($search_each) . "%'";
+                $implode[] = " xml LIKE '%" . $this->db->escape($search_each) . "%'";
             }
         }
 
@@ -96,7 +98,7 @@ class ModelExtensionModificationEditor extends Model {
                 $data['limit'] = 20;
             }
 
-            $sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+            $sql .= " LIMIT " . (int) $data['start'] . "," . (int) $data['limit'];
         }
 
         $query = $this->db->query($sql);
@@ -105,7 +107,7 @@ class ModelExtensionModificationEditor extends Model {
     }
 
     public function getExtensionInstallByExtensionInstallId($extension_install_id) {
-        $query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "extension_install` WHERE `extension_install_id` = '" . (int)$extension_install_id . "'");
+        $query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "extension_install` WHERE `extension_install_id` = '" . (int) $extension_install_id . "'");
 
         return $query->row;
     }
