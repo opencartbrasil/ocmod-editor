09a8c1ed5ab75a46bf75b6d5d77bf837cd26a13f
alx <alxjzx100@gmail.com>
2022-04-16 17:56:26 +0300

    Ukrainian and russian translations added.

---
+++ upload/admin/language/uk-ua/extension/modification/editor.php
@@ -0,0 +1,48 @@
+<?php
+// Заголовок
+$_['heading_title'] = 'Редактор модифікаторів';
+
+// Текст
+$_['text_success_edit'] = 'Відмінно: Модифікатор успішно змінено, кеш оновлено!';
+$_['text_success_add'] = 'Відмінно: Модифікатор успішно доданий, кеш оновлено!';
+$_['text_erase_theme'] = 'Відмінно: Кеш twig/sass скинуто!';
+$_['text_erase_data'] = 'Відмінно: Системний кеш скинуто!';
+$_['text_erase_image'] = 'Відмінно: Кеш зображень скинуто!';
+$_['text_loading'] = 'Збереження...';
+$_['text_erasing'] = 'Стирання...';
+$_['text_modifications'] = 'Модифікатори';
+$_['text_new'] = 'Новий модифікатор';
+$_['text_help_ocmod'] = '<strong>Допомога:</strong><br><a href="https://github.com/opencart/opencart/wiki/Modification-System" target="_blank" rel ="Manual Sys">Натисніть тут</a>, щоб перейти до документації модуля.';
+$_['text_xml_code'] = 'XML код';
+
+// Кнопки
+$_['button_log_error'] = 'Лог помилок';
+$_['button_files'] = 'Модифіковані файли';
+$_['button_new'] = 'Створити модифікатор';
+$_['button_edit'] = 'Редагувати';
+$_['button_download'] = 'Завантажити XML-файл';
+$_['button_erase_theme'] = 'Скинути кеш twig/sass';
+$_['button_erase_data'] = 'Скинути системний кеш';
+$_['button_erase_image'] = 'Скинути кеш зображень';
+$_['button_save'] = 'Зберегти';
+$_['button_return'] = 'Скасування';
+
+// Помилки
+$_['error_permission'] = 'Увага: У вас недостатньо повноважень!';
+$_['error_name_tag'] = 'Відсутній тег name!';
+$_['error_name_value'] = 'Тег name повинен бути заповнений!';
+$_['error_code_tag'] = 'Відсутній тег code!';
+$_['error_code_value'] = 'Тег code повинен бути заповнений!';
+$_['error_author_tag'] = 'Відсутній тег author!';
+$_['error_author_value'] = 'Тег author повинен бути заповнений!';
+$_['error_version_tag'] = 'Відсутній тег version!';
+$_['error_version_value'] = 'Тег version повинен бути заповнений!';
+$_['error_link_tag'] = 'Відсутній тег link!';
+$_['error_link_value'] = 'Тег link не може бути порожнім!';
+$_['error_file_tag'] = 'Відсутній тег file!';
+$_['error_modification_id'] = 'id модифікатора вказано неправильно!';
+$_['error_code_used'] = 'Модифікатор %s використовує той самий код!';
+$_['error_xml'] = 'XML містить помилки!';
+$_['error_file'] = 'XML не знайдено!';
+$_['error_headers'] = 'Помилка: заголовки вже надіслані!';
+$_['error_file_not_found'] = 'Файл ocmod.zip не знайдено!';
