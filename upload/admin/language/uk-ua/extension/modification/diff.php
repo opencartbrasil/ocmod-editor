200bced8f963ca488148720f6f9670d2d41b8451
alx <alxjzx100@gmail.com>
2022-04-16 17:43:48 +0300

    Ukrainian and russian translations added.

---
+++ upload/admin/language/uk-ua/extension/modification/diff.php
@@ -0,0 +1,18 @@
+<?php
+// Заголовок
+$_['heading_title'] = 'Аналіз модифікатора';
+
+// Текст
+$_['text_modifications'] = 'Модифікатори';
+$_['text_help_diff'] = '<strong>Допомога:</strong><br>Ліворуч - оригінальний файл, праворуч - модифікований.';
+$_['text_success_edit'] = '<strong><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Увага!</strong><br>Ви зберегли вміст правої панелі - це кешований файл OCMOD: <br><small><i>%s</i></small>';
+
+// Кнопки
+$_['button_save'] = 'Зберегти';
+$_['button_return'] = 'Скасувати';
+
+// Помилки
+$_['error_permission'] = 'Увага: У вас недостатньо прав для перегляду модифікатора!';
+$_['error_file'] = 'Файл не знайдено!';
+$_['error_code'] = 'Код відсутній!';
+$_['error_filewrite'] = 'Проблема при збереженні файлу!';
---
+++ upload/admin/language/uk-ua/extension/modification/search.php
@@ -0,0 +1,27 @@
+<?php
+// Заголовок
+$_['heading_title'] = 'Пошук XML';
+
+// Текст
+$_['text_modifications'] = 'Модифікатори';
+$_['text_search'] = 'Пошук XML-файлів';
+$_['text_no_results'] = 'Нічого не знайдено.';
+$_['text_file'] = 'Файл';
+$_['text_modification'] = 'Модифікатор';
+$_['text_version'] = 'Версія';
+$_['text_author'] = 'Автор';
+$_['text_search'] = 'Пошук XML';
+$_['text_list'] = 'Результати пошуку';
+
+// Поля
+$_['entry_name'] = 'Код для пошуку';
+$_['entry_search_name'] = 'Вставте код';
+
+// Колонки
+$_['column_name'] = 'Модифікатор';
+$_['column_date_added'] = 'Дата';
+$_['column_action'] = 'Дія';
+
+// Кнопки
+$_['button_return'] = 'Назад';
+$_['button_search_query'] = 'Пошук';
